613698b1cfce49454ed7b3f4c2c24fe70c201602
lixin <2690688423@qq.com>
2021-09-04 19:17:39 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n",)
+
+	repl.Start(os.Stdin, os.Stdout)
+}
+
+//let fib = fn(x){if(x<2){return x;}else{return fib(x-1) + fib(x-2);}};
